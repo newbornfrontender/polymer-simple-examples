b51a0b9b54ffcea01d5f95078403d1af09e45c5d
Infant Frontender <newbornfrontender@gmail.com>
2018-09-21 10:50:37 +0300

    Добавил роутинг и переключатели страниц

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -1,36 +1,93 @@
-import { LitElement, html } from '@polymer/lit-element';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element';
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings';
 
-class MyApp extends LitElement {
+import '@polymer/app-route/app-location';
+import '@polymer/app-route/app-route';
+import '@polymer/iron-pages/iron-pages';
+import '@polymer/iron-selector/iron-selector';
+
+setPassiveTouchGestures(true);
+setRootPath(MyAppGlobals.rootPath);
+
+class MyApp extends PolymerElement {
+  static get template () {
+    return html`
+      <app-location
+        route="{{route}}"
+        url-space-regex="^[[rootPath]]">
+      </app-location>
+
+      <app-route
+        route="{{route}}"
+        pattern="[[rootPath]]:page"
+        data="{{routeData}}"
+        tail="{{subroute}}">
+      </app-route>
+
+      <iron-selector
+        selected="[[page]]"
+        attr-for-selected="name"
+        role="navigation"
+      >
+        <a
+          name="home"
+          href="[[rootPath]]"
+        >Home</a>
+        <a
+          name="about"
+          href="[[rootPath]]about"
+        >About</a>
+      </iron-selector>
+
+      <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+        <my-view-home name="home"></my-view-home>
+        <my-view-about name="about"></my-view-about>
+        <my-view-404 name="404"></my-view-404>
+      </iron-pages>
+    `;
+  };
+
   static get properties () {
     return {
-      msg: String,
-      theme: String,
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_pageChanged',
+      },
+      routeData: Object,
+      subroute: Object,
     };
   };
 
-  constructor () {
-    super();
-
-    this.msg = 'default';
-    this.theme = 'green';
+  static get observers () {
+    return [
+      '_routePageChanged(routeData.page)',
+    ];
   };
 
-  render () {
-    return html`
-      <style>
-        span {
-          color: ${this.theme};
-        }
-      </style>
+  _routePageChanged (page) {
+    if (!page) {
+      this.page = 'home';
+    } else if (['about'].indexOf(page) !== -1) {
+      this.page = page;
+    } else {
+      this.page = '404';
+    };
+  };
 
-      <slot name="top-elem"></slot>
-      <h1>Component: <span>${this.msg}</span></h1>
-      <slot name="fallback">
-        <p>Fallback</p>
-      </slot>
-      <slot></slot>
-    `;
+  _pageChanged (page) {
+    switch (page) {
+      case 'home':
+        import('./views/my-view-home.js');
+        break;
+      case 'about':
+        import('./views/my-view-about.js');
+        break;
+      case '404':
+        import('./views/my-view-404.js');
+        break;
+    };
   };
 };
 
-customElements.define('my-app', MyApp);
+window.customElements.define('my-app', MyApp);
